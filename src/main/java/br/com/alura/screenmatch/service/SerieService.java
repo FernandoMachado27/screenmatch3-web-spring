44f9c079fe2af4b9b438a21707e93a3be3617c5f
FernandoMachado27 <fernando2747@yahoo.com>
2025-07-14 22:26:12 -0300

    Ajustando a busca de lançamentos

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -25,7 +25,7 @@ public class SerieService {
     }
 
     public List<SerieDTO> obterLancamentos() {
-        return converteDados(repository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+        return converteDados(repository.lancamentosMaisRecentes());
     }
 
     private List<SerieDTO> converteDados(List<Serie> series) { // recebe lista do tipo Série e converte em SérieDTO
